ae7657b75db0eebe5b5b20688750b5dfcd0c27c0
benchzak <benchzakaria3@gmail.com>
2025-07-16 19:21:53 +0100

    task2 : explore a simple routing in next.js

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,8 @@
+const Landing: React.FC = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  )
+}
+export default Landing
