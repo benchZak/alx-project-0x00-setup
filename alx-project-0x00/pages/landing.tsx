b28809ecf1ca7f7e231ac5dd90fbd27d3e47ea95
benchzak <benchzakaria3@gmail.com>
2025-07-16 19:34:26 +0100

    task3 : Rendering Components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,16 @@
+import Card from "@/components/Card"
+
 const Landing: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-8">
+      <h1 className="text-xl font-extralight mb-8">Landing Page</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   )
 }
+
 export default Landing
